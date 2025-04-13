6ecccc02d6c50726d019d77b06c694dd2e8c8fb2
kazido <beegee1803@gmail.com>
2025-04-12 23:54:46 -0400

    utilities

---
+++ utils/ascii_print.py
@@ -0,0 +1,8 @@
+def aprint(filepath: str) -> list:
+    """Reads ascii art lines from a file at filepath and returns a list of lines"""
+    temp = []
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            temp.append(line.strip("\n"))
+            
+    return temp
