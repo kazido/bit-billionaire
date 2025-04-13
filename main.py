cf55d03a84742f0393b9000d67852d8a20d7b4b9
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-04-13 12:57:25 -0400

    cleaned up!

--- main.py
+++ main.py
@@ -1,54 +1,30 @@
-from blessed import Terminal
-
-from menus._menu import Menu
-from menus.login import Login
-from menus.main_menu import MainMenu
-from menus.instructions import Instructions
-from menus.game import Game
+import argparse
+from custom.CTerminal import CTerminal
 
 from database.register import register
 
-register()
-                
+# Parse arguments from the command line
+parser = argparse.ArgumentParser(
+    prog="Bit-Billionaire",
+    description="Play the Bit-Billionaire game!",
+    epilog="Developed by bry400"
+)
+
+parser.add_argument('--register', action='store_true', help="register an account for the game")
+args = parser.parse_args()
+
+# If the register flag was passed, start account creation
+if args.register:
+    register()
+
 # blessed Terminal instance
-term = Terminal()
-
-# Master list of valid menus that we can navigate to.
-# TODO: These may not work as initialized off rip. May need to be non-init and called when navigated to.
-master = {
-    "LOGIN": Login(term),
-    "MAIN": MainMenu(term),
-    "INSTRUCTIONS": Instructions(term),
-    "GAME": Game(term),
-    "BACK": None,
-}
-
-# List of menu history used for navigation
-history = [master["LOGIN"]]
+term = CTerminal()
 
 while True:
     # Hide the cursor and allow input to be instantly read
     with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-
-        try:
-            menu: Menu = history[len(history) - 1]
-        except IndexError:
-            break  # We hit Q on main menu
-
+        
         # Run the current menu and set whatever it returns to the new menu
-        menu.run()
-
-        # Process navigation inputs after we have done everything in the screen
-        signal = menu.process_navigation()
-
-        # Handle signals at the highest level
-        if signal not in master.keys():
-            continue  # AKA: Ignore random keypresses
-
-        if signal == "BACK":
-            # Signaled to go back, remove current menu from history.
-            history.pop()
-
-        else:
-            # Valid navigation, add new menu to the history.
-            history.append(master[signal])
+        term.current_menu.run()
+        
+        term.update()
